f78d9a5d0c0537bdaf1c6183a2a7058a67e852f5
maxpro <mprocopenco@mixbook.com>
2020-04-17 10:08:11 +0300

    Add super+tab shortcut to go to next client

--- rc.lua
+++ rc.lua
@@ -86,6 +86,8 @@ awful.screen.connect_for_each_screen(
 globalkeys = gears.table.join(
                awful.key({modkey}, "s", hotkeys_popup.show_help, {description = "show help", group = "awesome"}),
                  awful.key(
+                   {modkey}, "Tab", function() awful.client.focus.byidx(1) end,
+                     {description = "focus next by index", group = "client"}), awful.key(
                    {modkey}, "Right", function() awful.client.focus.byidx(1) end,
                      {description = "focus next by index", group = "client"}), awful.key(
                    {modkey}, "Left", function() awful.client.focus.byidx(-1) end,
@@ -100,11 +102,6 @@ globalkeys = gears.table.join(
                      {description = "focus the previous screen", group = "screen"}), awful.key(
                    {modkey}, "u", awful.client.urgent.jumpto, {description = "jump to urgent client", group = "client"}),
                  awful.key(
-                   {modkey}, "Tab", function()
-        awful.client.focus.history.previous()
-        if client.focus then client.focus:raise() end
-      end, {description = "go back", group = "client"}), -- Standard program
-    awful.key(
                    {modkey}, "Return", function() awful.spawn(terminal) end,
                      {description = "open a terminal", group = "launcher"}), awful.key(
                    {modkey, "Control"}, "r", awesome.restart, {description = "reload awesome", group = "awesome"}),
